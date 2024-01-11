490aa2725f09b8b9d50f78049fe1c7a2c2b885e9
eXclus1on <rodolfo.fidalgo746@gmail.com>
2024-01-11 21:02:26 +0000

    feat: project-setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Register.js
@@ -0,0 +1,46 @@
+import React from "react";
+import logo from "d:/_Front-end-project/lingua-gestual-base/src/images/ivling.png"; // Importa o logotipo (ajusta o caminho conforme necessário)
+import "./Register.css";
+import Input from "./Input";
+
+const FORM_INPUTS = [
+  { label: "Nome Completo", id: "username", name: "username", type: "text" },
+  { label: "Número de Identificação Fiscal", id: "idFiscal", name: "idFiscal", type: "text" },
+  { label: "Password", id: "password", name: "password", type: "password" },
+  { label: "Confirmar Password", id: "confirmPassword", name: "confirmPassword", type: "password" },
+];
+
+const Register = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Adicione lógica para processar o formulário aqui, se necessário
+  };
+
+  return (
+    <div className="register-container">
+      <img src={logo} alt="Logo" className="logo" />
+      <div className="wrapper">
+        <form onSubmit={handleSubmit}>
+          {FORM_INPUTS.map((entry, index) => {
+            const { label, ...rest } = entry;
+
+            return (
+                
+              <Input {...rest} key={`${index}`}>
+              <br></br>
+                {label}
+                <br></br>
+              </Input>
+            );
+          })}
+          <br></br>
+          <button className="button-submit" type="submit">
+            Submeter
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
